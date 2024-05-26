ffcb499d827a44a06c5f7b49780ce08fbf83f52f
The Black Wizard <chuckydu24@gmail.com>
2024-05-26 21:54:19 +0200

    chore(variable): add helper

--- inc/functions/Import-Variables.ps1
+++ inc/functions/Import-Variables.ps1
@@ -1,4 +1,32 @@
 function Import-Variables {
+    <#
+    .SYNOPSIS
+        Summary of the script
+    .DESCRIPTION
+        Script description
+    .PARAMETER param1
+        Parameter description
+    .INPUTS
+        Pipeline input data
+    .OUTPUTS
+        Output data
+    .EXAMPLE
+        .\template.ps1 param1
+    .NOTES
+        Name           : Script-Name
+        Version        : 1.0.0
+        Created by     : Chucky2401
+        Date Created   : 11/04/2023
+        Modify by      : Chucky2401
+        Date modified  : 11/04/2023
+        Change         : Creation
+        Copy           : Copy-Item .\Script-Name.ps1 \Final\Path\Script-Name.ps1 -Force
+    .LINK
+        http://github.com/UserName/RepoName
+    #>
+
+    #------------------------------------------------------------ [Parameters] ------------------------------------------------------------
+
     [CmdletBinding(SupportsShouldProcess, ConfirmImpact = "Low")]
     Param (
         [Parameter(Mandatory = $True)]
@@ -7,7 +35,15 @@ function Import-Variables {
         [String]$Scope = "Script"
     )
 
+    #---------------------------------------------------------- [Initialisation] ----------------------------------------------------------
+
+    #------------------------------------------------------------ [Functions] -------------------------------------------------------------
+
+    #----------------------------------------------------------- [Declaration] ------------------------------------------------------------
+
     $prefix = (Split-Path -Leaf D:\Utilisateurs\TheBlackWizard\Logiciels\Git\PowerShell\hass-powershell\inc\vars\mqtt).ToUpper()
+
+    #------------------------------------------------------------ [Execution] -------------------------------------------------------------
 
     Get-Content $FilePath | Foreach-Object {
         $variable = $PSItem.Split('=')
