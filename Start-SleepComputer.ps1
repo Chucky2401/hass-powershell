14219a18b7bfa21c56b7c95ec20be559a3200767
The Black Wizard <chuckydu24@gmail.com>
2024-05-26 20:22:39 +0200

    feat(sleep): turn off RGB
    Use OpenRGB with the profile 'Eteint' (turn off) to turn off RGB

--- Start-SleepComputer.ps1
+++ Start-SleepComputer.ps1
@@ -43,20 +43,27 @@ If ($PSBoundParameters['Debug']) {
 
 Add-Type -Assembly System.Windows.Forms
 Add-Type -Path "D:\Utilisateurs\TheBlackWizard\Logiciels\Git\PowerShell\hass-powershell\lib\M2Mqtt.4.3.0.0\lib\net45\M2Mqtt.Net.dll"
+
 $scriptRoot = Split-Path $Script:MyInvocation.MyCommand.Path
 
 #-----------------------------------------------------------[Functions]------------------------------------------------------------
 
 . $scriptRoot\inc\functions\Import-Variables.ps1
+
 #----------------------------------------------------------[Declarations]----------------------------------------------------------
 
 Import-Variables -FilePath $scriptRoot\inc\vars\mqtt
+
 $finalState = [System.Windows.Forms.PowerState]::Suspend
 
 $mqttClient = [uPLibrary.Networking.M2Mqtt.MqttClient]($MQTT_SERVER)
+
 #-----------------------------------------------------------[Execution]------------------------------------------------------------
 
 $mqttClient.Connect([guid]::NewGuid(), $MQTT_USERNAME, $MQTT_PASSWORD)
 $mqttClient.Publish("M2MQTTPowershell/Zoukzouk/message", [System.Text.Encoding]::UTF8.GetBytes("Start Suspend"))
 $mqttClient.Disconnect()
+
+Start-Process -FilePath "D:\Utilisateurs\TheBlackWizard\Logiciels\OpenRGB Windows 64-bit\OpenRGB.exe" -ArgumentList "--profile Eteint.orp" -Wait -NoNewWindow
+
 [System.Windows.Forms.Application]::SetSuspendState($finalState, $false, $false) | Out-Null
